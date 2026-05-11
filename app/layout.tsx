f6e11b9c83d855e563bc468ec5b78d6acc9d149b
dizzybolt <dizzybolt@naver.com>
2026-05-11 17:00:34 +0900

    app title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Private AI Chat",
-  description: "Private character AI chat service",
+  title: "YATA AI Chat",
+  description: "YATA AI chat service",
 
   applicationName: "YATA AI Chat",
 
